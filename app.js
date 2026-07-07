0ae0fca5b60f3d6c0006e0bef105969852e77988
lRethi <azraethly@gmail.com>
2026-07-07 10:26:18 -0300

    2.0.0
    Adicionado algumas novas funções e verificações, como armazenar o aluno emprestando o livro para que só ele possa devolver, e o de ser necessário estar logado para poder ter as permissões de devolver e emprestar livros.

--- app.js
+++ app.js
@@ -3,31 +3,33 @@ const app = express();
 
 app.use(express.json());
 
+var usuarioAutenticado = false;
+
 const livros = [
-    { id: 1, nome: 'Chainsaw Man - Vol. 2', genero: 'manga', disponibilidade: true },
-    { id: 2, nome: 'Berserk - Vol. 1', genero: 'manga', disponibilidade: true },
-    { id: 3, nome: 'Death Note - Black Edition Vol. 1', genero: 'manga', disponibilidade: true },
-    { id: 4, nome: 'Vinland Saga - Vol. 1', genero: 'manga', disponibilidade: true },
-    { id: 5, nome: 'Duna', genero: 'ficcao', disponibilidade: true },
-    { id: 6, nome: 'Neuromancer', genero: 'ficcao', disponibilidade: true },
-    { id: 7, nome: 'Fundação', genero: 'ficcao', disponibilidade: true },
-    { id: 8, nome: 'Admirável Mundo Novo', genero: 'ficcao', disponibilidade: true },
-    { id: 9, nome: '1984', genero: 'ficcao', disponibilidade: true },
-    { id: 10, nome: 'O Festim dos Corvos', genero: 'fantasia', disponibilidade: true },
-    { id: 11, nome: 'O Senhor dos Anéis: A Sociedade do Anel', genero: 'fantasia', disponibilidade: true },
-    { id: 12, nome: 'O Nome do Vento', genero: 'fantasia', disponibilidade: true },
-    { id: 13, nome: 'Mistborn: O Império Final', genero: 'fantasia', disponibilidade: true },
-    { id: 14, nome: 'O Hobbit', genero: 'fantasia', disponibilidade: true },
-    { id: 15, nome: 'UNIX: Uma Revolução Informática', genero: 'cientifico', disponibilidade: true },
-    { id: 16, nome: 'Código Limpo', genero: 'cientifico', disponibilidade: true },
-    { id: 17, nome: 'Entendendo Algoritmos', genero: 'cientifico', disponibilidade: true },
-    { id: 18, nome: 'O Programador Pragmático', genero: 'cientifico', disponibilidade: true },
-    { id: 19, nome: 'The C++ Programming Language', genero: 'cientifico', disponibilidade: true },
-    { id: 20, nome: 'House of Leaves', genero: 'terror', disponibilidade: true },
-    { id: 21, nome: 'O Iluminado', genero: 'terror', disponibilidade: true },
-    { id: 22, nome: 'Drácula', genero: 'terror', disponibilidade: true },
-    { id: 23, nome: 'Frankenstein', genero: 'terror', disponibilidade: true },
-    { id: 24, nome: 'A Assombração da Casa da Colina', genero: 'terror', disponibilidade: true }
+    { id: 1, nome: 'Chainsaw Man - Vol. 2', genero: 'manga', disponibilidade: true, aluno: '-' },
+    { id: 2, nome: 'Berserk - Vol. 1', genero: 'manga', disponibilidade: true, aluno: '-' },
+    { id: 3, nome: 'Death Note - Black Edition Vol. 1', genero: 'manga', disponibilidade: true, aluno: '-' },
+    { id: 4, nome: 'Vinland Saga - Vol. 1', genero: 'manga', disponibilidade: true, aluno: '-' },
+    { id: 5, nome: 'Duna', genero: 'ficcao', disponibilidade: true, aluno: '-' },
+    { id: 6, nome: 'Neuromancer', genero: 'ficcao', disponibilidade: true, aluno: '-' },
+    { id: 7, nome: 'Fundação', genero: 'ficcao', disponibilidade: true, aluno: '-' },
+    { id: 8, nome: 'Admirável Mundo Novo', genero: 'ficcao', disponibilidade: true, aluno: '-' },
+    { id: 9, nome: '1984', genero: 'ficcao', disponibilidade: true, aluno: '-' },
+    { id: 10, nome: 'O Festim dos Corvos', genero: 'fantasia', disponibilidade: true, aluno: '-' },
+    { id: 11, nome: 'O Senhor dos Anéis: A Sociedade do Anel', genero: 'fantasia', disponibilidade: true, aluno: '-' },
+    { id: 12, nome: 'O Nome do Vento', genero: 'fantasia', disponibilidade: true, aluno: '-' },
+    { id: 13, nome: 'Mistborn: O Império Final', genero: 'fantasia', disponibilidade: true, aluno: '-' },
+    { id: 14, nome: 'O Hobbit', genero: 'fantasia', disponibilidade: true, aluno: '-' },
+    { id: 15, nome: 'UNIX: Uma Revolução Informática', genero: 'cientifico', disponibilidade: true, aluno: '-' },
+    { id: 16, nome: 'Código Limpo', genero: 'cientifico', disponibilidade: true, aluno: '-' },
+    { id: 17, nome: 'Entendendo Algoritmos', genero: 'cientifico', disponibilidade: true, aluno: '-' },
+    { id: 18, nome: 'O Programador Pragmático', genero: 'cientifico', disponibilidade: true, aluno: '-' },
+    { id: 19, nome: 'The C++ Programming Language', genero: 'cientifico', disponibilidade: true, aluno: '-' },
+    { id: 20, nome: 'House of Leaves', genero: 'terror', disponibilidade: true, aluno: '-' },
+    { id: 21, nome: 'O Iluminado', genero: 'terror', disponibilidade: true, aluno: '-' },
+    { id: 22, nome: 'Drácula', genero: 'terror', disponibilidade: true, aluno: '-' },
+    { id: 23, nome: 'Frankenstein', genero: 'terror', disponibilidade: true, aluno: '-' },
+    { id: 24, nome: 'A Assombração da Casa da Colina', genero: 'terror', disponibilidade: true, aluno: '-' }
 ];
 
 const USUARIO_PADRAO = 'admin';
@@ -63,6 +65,8 @@ app.post('/login', (req, res) => {
     } else {
         res.status(401).json({ erro: 'Usuário ou senha incorretos.' });
     }
+
+    usuarioAutenticado = true;
 });
 
 function buscarLivro(id) {
@@ -78,6 +82,8 @@ function buscarLivro(id) {
 app.post('/emprestimo', (req, res) => {
     const id = req.body.id;
     const aluno = req.body.aluno;
+
+    if (!usuarioAutenticado) return res.status(401).json({ erro: 'Usuário conectado não tem autorização para essa ação.' });
 
     if (!id || !aluno) {
         return res.status(400).json({ erro: 'Envie id e aluno.' });
@@ -96,6 +102,7 @@ app.post('/emprestimo', (req, res) => {
     }
 
     livro.disponibilidade = false;
+    livro.aluno = aluno;
     res.status(201).json({
         mensagem: 'Emprestimo do livro ' + livro.nome + ' registrado para ' + aluno,
         livro: livro.nome,
@@ -105,6 +112,9 @@ app.post('/emprestimo', (req, res) => {
 app.post('/devolucao', (req, res) => {
     const id = req.body.id;
     const aluno = req.body.aluno;
+
+    if (!usuarioAutenticado) return res.status(401).json({ erro: 'Usuário conectado não tem autorização para essa ação.' });
+
 
     if (!id || !aluno) {
         return res.status(400).json({ erro: 'Envie id e aluno.' });
@@ -122,11 +132,18 @@ app.post('/devolucao', (req, res) => {
         });
     }
 
+    if (livro.aluno != '-' && livro.aluno != aluno) {
+        return res.status(400).json({
+            erro: 'Livro emprestado por outro aluno'
+        });
+    }
+
     livro.disponibilidade = true;
     res.status(201).json({
         mensagem: 'Devolução do livro ' + livro.nome + ' feita por ' + aluno,
         livro: livro.nome,
     });
+    livro.aluno = '-';
 });
 
 app.listen(3001, () => {
